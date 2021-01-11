f99e70627b4fecacb2c7050048bb9c818b8097ab
Shohanur Rahman <shohanur.rahman57@gmail.com>
2021-01-11 17:04:54 +0600

    update

--- resources/views/user/partials/widget/brand_slider.blade.php
+++ resources/views/user/partials/widget/brand_slider.blade.php
@@ -3,19 +3,21 @@
 @endphp
 
 @if(count($brandSliders) > 0)
-<div class="container-fluid mt-60">
-    <div class="brands-area">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="brand-items">
-                    @foreach($brandSliders as $brandSlider)
-                        <div class="brand-item">
-                            <img class="brand-static" src="{{asset($brandSlider->image)}}" alt="brand images"/>
+    <div class="container-fluid mt-60">
+        <div class="brands-area">
+            <div class="row">
+                <div class="card border-0 py-4 px-4">
+                    <div class="col-lg-12">
+                        <div class="brand-items">
+                            @foreach($brandSliders as $brandSlider)
+                                <div class="brand-item">
+                                    <img class="brand-static" src="{{asset($brandSlider->image)}}" alt="brand images"/>
+                                </div>
+                            @endforeach()
                         </div>
-                    @endforeach()
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endif
--- resources/views/user/partials/widget/brands_slider.blade.php
+++ resources/views/user/partials/widget/brands_slider.blade.php
@@ -1,49 +1,50 @@
-
 <!--brands-area start-->
 <div class="container-fluid mt-60">
     <div class="brands-area">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="brand-items">
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/1.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/2.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/3.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/4.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/5.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/6.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/7.jpg" alt=""/>
-                        </a>
-                    </div>
-                    <div class="brand-item">
-                        <a href="#">
-                            <img class="brand-static" src="/user/assets/images/brands/8.jpg" alt=""/>
-                        </a>
+            <div class="card border-0 py-4 px-4">
+                <div class="col-lg-12">
+                    <div class="brand-items">
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/1.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/2.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/3.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/4.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/5.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/6.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/7.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="brand-item">
+                            <a href="#">
+                                <img class="brand-static" src="/user/assets/images/brands/8.jpg" alt=""/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
